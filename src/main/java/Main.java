62c16d08365b6c1aa0c9c2e07a543fed26859620
omegaspard <gdiopdub@gmail.com>
2020-09-29 08:51:58 -0700

    Add a menu to chose the board size.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // TODO: Make a menu to choose the board size.
+        System.out.println("Simple snake in java.");
+        System.out.println("The original snake game coded in java.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String regexOnlyNumbers = "[0-9]+";
+        boolean regexMatch = false;
         int boardSize = 3;
 
+        do {
+            System.out.println("Chose your board size: (enter a number between 3 and 30, default is 3)");
+            String boardSizeUserInput = scanner.nextLine();
+
+            if(boardSizeUserInput.isEmpty()) break;
+
+            regexMatch = boardSizeUserInput.matches(regexOnlyNumbers);
+            boardSize = Integer.parseInt(boardSizeUserInput);
+
+        }while(!regexMatch || boardSize < 3 || boardSize > 30);
+
+        System.out.println("The board size is set to " + boardSize);
         SnakeGameController.run(boardSize);
     }
 }
